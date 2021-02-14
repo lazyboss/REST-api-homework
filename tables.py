5d7b9211a265570f8c85c3912abe9c6d64052892
Tahzeeb <44545675+lazyboss@users.noreply.github.com>
2021-02-14 21:04:26 +0530

    Update tables.py

--- tables.py
+++ tables.py
@@ -3,6 +3,7 @@ from flask_table import Table, Col, LinkCol
 class Results(Table):
     user_id = Col('Id', show=False)
     user_name = Col('Name')
+    user_age = Col('Age')
     user_email = Col('Email')
     user_password = Col('Password', show=False)
     edit = LinkCol('Edit', 'edit_view', url_kwargs=dict(id='user_id'))
